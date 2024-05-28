fffd7bddcd0087874968f81a23ba04de569be93b
Rafael Coelho <rafaelcoelho3110@gmail.com>
2024-05-27 23:48:51 -0300

    fix: corrigindo testes

--- src/test/kotlin/br/group/twenty/challenge/tech/domain/services/CreateOrderAdapterTest.kt
+++ src/test/kotlin/br/group/twenty/challenge/tech/domain/services/CreateOrderAdapterTest.kt
@@ -3,6 +3,7 @@ package br.group.twenty.challenge.tech.domain.services
 import br.group.twenty.challenge.domain.models.order.CreateOrderModel
 import br.group.twenty.challenge.domain.models.order.Order
 import br.group.twenty.challenge.infra.adapters.order.CreateOrderAdapter
+import br.group.twenty.challenge.infra.adapters.product.FindProductAdapter
 import br.group.twenty.challenge.infra.models.OrderEntity
 import br.group.twenty.challenge.infra.repositories.OrderJpaRepository
 import io.mockk.every
@@ -16,42 +17,45 @@ import java.time.LocalDateTime
 
 class CreateOrderAdapterTest {
     private val orderRepositoryMock = mockk<OrderJpaRepository>()
-//    private val createOrderAdapter = CreateOrderAdapter(orderRepositoryMock)
+    private val findProductAdapter = mockk<FindProductAdapter>()
+    private val createOrderAdapter = CreateOrderAdapter(orderRepositoryMock, findProductAdapter)
 
-//    @Test
-//    fun `Should throw an error if the order already exists`() {
-//        val input = CreateOrderModel(orderValue = BigDecimal("100.00"), idCustomer = 1, status = "PENDING")
-//        every { orderRepositoryMock.save(any()) } throws Exception("Order already exists")
-//
-//        val exception = assertThrows<Exception> {
-//            createOrderAdapter.createOrder(input)
-//        }
-//        assertEquals("Order already exists", exception.message)
-//    }
+    @Test
+    fun `Should throw an error if the order already exists`() {
+        val input = CreateOrderModel(idCustomer = 1, productList = mutableListOf())
+        every { orderRepositoryMock.save(any()) } throws Exception("Order already exists")
 
-//    @Test
-//    fun `Should register an order`() {
-//        val input = CreateOrderModel(orderValue = BigDecimal("100.00"), idCustomer = 1, status = "PENDING")
-//        val orderEntity = OrderEntity(
-//            idOrder = 1,
-//            orderValue = BigDecimal("100.00"),
-//            idCustomer = 1,
-//            creationOrder = LocalDateTime.now(),
-//            lastUpdateOrder = LocalDateTime.now(),
-//            status = "PENDING"
-//        )
-//        val expectedOutput = OrderEntity(
-//            idOrder = 1,
-//            orderValue = BigDecimal("100.00"),
-//            idCustomer = 1,
-//            creationOrder = orderEntity.creationOrder,
-//            lastUpdateOrder = orderEntity.lastUpdateOrder,
-//            status = "PENDING"
-//        )
-//
-//        every { orderRepositoryMock.save(any()) } returns orderEntity
-//
-//        val output = createOrderAdapter.createOrder(input)
-//        assertEquals(expectedOutput, output)
-//    }
+        val exception = assertThrows<Exception> {
+            createOrderAdapter.createOrder(input)
+        }
+        assertEquals("Order already exists", exception.message)
+    }
+
+    @Test
+    fun `Should register an order`() {
+        val input = CreateOrderModel(idCustomer = 1, productList = mutableListOf())
+        val orderEntity = OrderEntity(
+            idOrder = 1,
+            orderValue = BigDecimal("100.00"),
+            idCustomer = 1,
+            creationOrder = LocalDateTime.now(),
+            lastUpdateOrder = LocalDateTime.now(),
+            status = "PENDING",
+            mutableListOf()
+        )
+        val expectedOutput = OrderEntity(
+            idOrder = 1,
+            orderValue = BigDecimal("100.00"),
+            idCustomer = 1,
+            creationOrder = orderEntity.creationOrder,
+            lastUpdateOrder = orderEntity.lastUpdateOrder,
+            status = "PENDING",
+            mutableListOf()
+        )
+
+        every { orderRepositoryMock.save(any()) } returns orderEntity
+
+        val output = createOrderAdapter.createOrder(input)
+        assertEquals(expectedOutput, output)
+    }
 }
